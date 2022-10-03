cdd6bba644fcd0a7af3332d17eec664d32e44d81
RajVaibhav <raj.vaibhav@tothenew.com1>
2022-10-03 10:28:39 +0530

    Revert "A5 and A4 pending"
    This reverts commit 2d859fce9af747baa07bf94a2f03bb209afe1c0c.

--- Assigment5/src/Question4.java
+++
@@ -1,9 +0,0 @@
-import java.util.stream.IntStream;
-
-public class Question4 {
-
-    public static void main(String args[]) {
-
-        IntStream.iterate(1, i -> i<5, i -> i+1).forEach(System.out::println);
-    }
-}
